05c4ed49b9ce400cec9c6a3c2c0e78efdcfc961c
Josh Wood <jwood@swood.org>
2022-08-30 19:31:15 -0500

    added routing commands

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # root route at books/index
+  root "books#index"
+
+  # books route
   resources :books
+
+  # default route
+  get ":controller(/:action(:id))"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
