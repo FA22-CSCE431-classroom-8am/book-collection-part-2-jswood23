aa14aaa0681f1cdf9777de4bbfc2356be547c2dc
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-14 16:54:18 -0500

    Added new attributes to integration tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,9 +5,18 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J.K. Rowling'
+    fill_in 'Price', with: '6.98'
+    select '1997', from: 'book_published_1i'
+    select 'June', from: 'book_published_2i'
+    select '26', from: 'book_published_3i'
     click_on 'Create Book'
     visit books_path
+    click_on 'Show'
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('J.K. Rowling')
+    expect(page).to have_content('6.98')
+    expect(page).to have_content('1997-06-26')
   end
 end
 
