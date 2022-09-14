f85bde321ad7b691897e67e99964f622e9658dbf
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-14 16:55:01 -0500

    created new unit tests for new book attributes

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter', author: "J.K. Rowling", price: 6.98, published: 1997-06-26)
+    described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: 6.98, published: 1997-06-26)
   end
 
   it 'is valid with valid attributes' do
@@ -12,6 +12,24 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.title = 'harry potter'
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.author = 'J.K. Rowling'
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a publishing date' do
+    subject.price = 6.98
+    subject.published = nil
     expect(subject).not_to be_valid
   end
 end
