8f884dc6bf03b67aab5a954d333890116d30371d
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-16 13:25:03 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  resources :user_books
-  resources :users
   # root route at books/index
-  root "books#index"
+  root "user_books#index"
 
   # books route
   resources :books
+  resources :user_books
+  resources :users
 
-  # default route
-  get ":controller(/:action(:id))"
+  resources books do
+    member do
+      get :delete
+    end
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
