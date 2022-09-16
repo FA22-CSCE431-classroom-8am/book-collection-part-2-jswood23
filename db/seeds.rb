3fed1d0ee4e7e305533a969af236d62f1c2221c7
jswood23 <56269953+jswood23@users.noreply.github.com>
2022-09-16 14:01:42 -0500

    created seeds for users and books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+books = Book.create([
+  {
+    title: 'Atlas Shrugged',
+    author: 'Ayn Rand',
+    price: 8.15,
+    published: '2005-04-21'
+  },
+  {
+    title: 'Harry Potter',
+    author: 'J.K. Rowling',
+    price: 6.98,
+    published: '1997-06-26'
+  },
+  {
+    title: 'Brave New World',
+    author: 'Aldous Huxley',
+    price: 5.99,
+    published: '1932-10-18'
+  }
+])
+
+users = User.create([
+  { username: 'jswood' },
+  { username: 'howdy' },
+  { username: 'maroon' },
+  { username: 'whoop' }
+])
